dad60dc00fa310ac79aba0df5448de5eb16f6d81
Juliana Bonardi <106901927+Juh-B@users.noreply.github.com>
2024-12-04 13:43:36 -0300

    Reorganizing all files

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,51 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 11:11:28 by jcosta-b          #+#    #+#             */
+/*   Updated: 2024/12/04 13:15:42 by jcosta-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_printf.h"
 
-static size_t ft_specifiers(va_list args, char c)
+static size_t	printf_specifiers(va_list args, char c)
 {
-  size_t  printed;
+	size_t	printed;
 
-  printed = 0;
-  if (c == 'c')
-    printed += ft_putchar_fd(va_arg(args, int), 1);
-  else if (c == 's')
-    printed += ft_putstr_fd(va_arg(args, char *), 1);
-  else if (c == 'd' || c == 'i')
-    printed += ft_putnbr_fd(va_arg(args, int), 1);
-  else if (c == 'u')
-    printed += ft_putnbr_unsig_fd(va_arg(args, unsigned int), 1);
-  else if (c == 'x' || c == 'X')
-    printed += ft_puthex_fd(va_arg(args, unsigned int), c, 1);
-  else if (c == 'p')
-    printed += ft_putptr(args, c);
-  else if (c == '%')
-    printed += ft_putchar_fd('%', 1);
-  return (printed);
+	printed = 0;
+	if (c == 'c')
+		printed += printf_putchar(va_arg(args, int));
+	else if (c == 's')
+		printed += printf_putstr(va_arg(args, char *));
+	else if (c == 'd' || c == 'i')
+		printed += printf_putnbr(va_arg(args, int));
+	else if (c == 'u')
+		printed += printf_putnbr_unsig(va_arg(args, unsigned int));
+	else if (c == 'x' || c == 'X')
+		printed += printf_puthex(va_arg(args, unsigned int), c);
+	else if (c == 'p')
+		printed += printf_putptr(args, c);
+	else if (c == '%')
+		printed += printf_putchar('%');
+	return (printed);
 }
 
-// Funcao principal
-int ft_printf(const char *fmt_str, ...)
+int	ft_printf(const char *fmt_str, ...)
 {
-  va_list args;
-  int  i;
-  size_t  printed;
+	va_list	args;
+	int		i;
+	size_t	printed;
 
-  va_start(args, fmt_str);
-  i = 0;
-  printed = 0;
-  while (fmt_str[i] != '\0')
-  {
-    if (fmt_str[i] == '%' && fmt_str[i + 1] != '\0')
-    {
-      i++;
-      printed += ft_specifiers(args, fmt_str[i]);
-      i++;
-    }
-    else
-    {
-      printed += ft_putchar_fd(fmt_str[i], 1);
-      i++;
-    }
-  }
-  va_end(args);
-  return (printed);
+	va_start(args, fmt_str);
+	i = 0;
+	printed = 0;
+	while (fmt_str[i] != '\0')
+	{
+		if (fmt_str[i] == '%' && fmt_str[i + 1] != '\0')
+		{
+			i++;
+			printed += printf_specifiers(args, fmt_str[i]);
+			i++;
+		}
+		else
+		{
+			printed += printf_putchar(fmt_str[i]);
+			i++;
+		}
+	}
+	va_end(args);
+	return (printed);
 }
